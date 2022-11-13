df19c8e620f4a064283f0d914469cf0470e12bac
ErnestAV <59626561+ErnestAV@users.noreply.github.com>
2022-11-12 17:58:14 -0700

    Adding ModelCarousel and Company Gallery

---
+++ hackathon/src/components/CompanyGallery.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="m-20">
+    <h1 class="flex justify-center font-bold text-5xl text-center">
+      The #1 platform to connect companies and models
+    </h1>
+    <div class="flex flex-row space-x-10 my-5 justify-center">
+        <div>
+            <img src="../../public/images/companiesGallery/cotopaxi.webp" width="200" height="auto" alt="cotopaxi">
+        </div>
+        <div>
+            <img src="../../public/images/companiesGallery/naniswimwear.png" width="200" height="auto" alt="cotopaxi">
+        </div>
+        <div>
+            <img src="../../public/images/companiesGallery/piperscoot.webp" width="200" height="auto" alt="cotopaxi">
+        </div>
+        <div>
+            <img src="../../public/images/companiesGallery/soel.png" width="200" height="auto" alt="cotopaxi">
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompanyGallery',
+  // props: {
+  //   msg: String
+  // }
+}
+</script>
--- hackathon/src/views/Home.vue
+++ hackathon/src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home w-full">
     <Banner/>
+    <CompanyGallery/>
+    <ModelsCarousel/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Banner from '@/components/Banner.vue'
+import CompanyGallery from '@/components/CompanyGallery.vue'
+import ModelsCarousel from '@/components/ModelsCarousel.vue'
 
 export default {
   name: 'Home',
   components: {
     Banner,
+    CompanyGallery,
+    ModelsCarousel
   }
 }
 </script>
