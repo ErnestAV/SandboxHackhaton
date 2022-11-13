69c605dd08ff9ac979ac27428a47d5c2d7dc6341
TJ Maxwell <tjmaxwell157@gmail.com>
2022-11-12 17:09:10 -0700

    model page and card plus button

---
+++ hackathon/src/components/CardList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="w-full">
+        <div class="mt-4 justify-center items-center w-full flex flex-wrap">
+            <!-- <div v-for="model in showList" :key="model">
+                <ModelCard :model="model" />
+            </div> -->
+            <div class="w-1/3 p-5">
+                <ModelCard />
+            </div>
+            <div class="w-1/3 p-5">
+                <ModelCard />
+            </div>
+            <div class="w-1/3 p-5">
+                <ModelCard />
+            </div>
+            <div class="w-1/3 p-5">
+                <ModelCard />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ModelCard from './ModelCard.vue'
+    export default {
+        name: 'CardList',
+        props: {
+            models: {
+                type: Array,
+            },
+        },
+        components: {
+            ModelCard,
+        },
+        computed: {
+            showList() {
+                return JSON.parse(JSON.stringify(this.modelList));
+            },
+        },
+
+    };
+</script>
